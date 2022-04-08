7780c3aa9982b7d70a5f084b737c7d6038194701
p-chanmin <p_cm111@naver.com>
2022-04-08 21:10:26 +0900

    Update duration time

--- app/src/main/java/kr/ac/tukorea/android/earalarm/SplashActivity.kt
+++ app/src/main/java/kr/ac/tukorea/android/earalarm/SplashActivity.kt
@@ -7,7 +7,7 @@ import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
-    val DURATION : Long = 3000
+    val DURATION : Long = 1500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
