9791a04f9c4cf239f36659f7754d5542d2309104
p-chanmin <pc_m111@naver.com>
2024-05-22 22:15:54 +0900

    build: lib 추가 및 version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,4 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import java.io.FileInputStream
-import java.util.Properties
 
 plugins {
     id("com.android.application")
@@ -65,20 +63,23 @@ fun getPropertyKey(propertyKey: String): String {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
 
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
-    implementation("com.google.android.gms:play-services-ads:22.6.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
 }
