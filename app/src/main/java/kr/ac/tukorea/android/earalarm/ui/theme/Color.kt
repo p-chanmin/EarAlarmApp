6cf205032d0eef23fb106be8803733b9e7fa5325
p-chanmin <pc_m111@naver.com>
2024-06-14 22:08:16 +0900

    feat: compose color 추가

---
+++ app/src/main/java/kr/ac/tukorea/android/earalarm/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package kr.ac.tukorea.android.earalarm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Ivory = Color(0xFFFFFCEE)
+val BlackOlive = Color(0xFF403F3B)
+val DimGray = Color(0xFF676767)
+val White = Color(0xFFFFFFFF)
+val LightGreen = Color(0xFF43AA8B)
+val Silver = Color(0xFFBFBDB2)
