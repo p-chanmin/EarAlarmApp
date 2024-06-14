a4a7342b2bf7e5565f42e360c7323284adf72faf
p-chanmin <pc_m111@naver.com>
2024-06-14 20:48:54 +0900

    build: compose dependencies 추가, 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ android {
             isDebuggable = false
         }
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -52,6 +55,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        compose = true
         dataBinding = true
         buildConfig = true
     }
@@ -64,22 +68,41 @@ fun getPropertyKey(propertyKey: String): String {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Compose
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.ui:ui-graphics:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-android-compiler:2.49")
+
+    // Datastore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Google Admob
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
-
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
-
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-
-    implementation("com.google.android.gms:play-services-ads:23.1.0")
-
-    implementation("com.google.code.gson:gson:2.10.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
