524020462491917019b40744a3c4afdc2ec3fb64
p-chanmin <pc_m111@naver.com>
2024-06-16 23:17:01 +0900

    feat: PrimaryButton ui component 추가

---
+++ app/src/main/java/kr/ac/tukorea/android/earalarm/ui/components/buttons/PrimaryButton.kt
@@ -0,0 +1,60 @@
+package kr.ac.tukorea.android.earalarm.ui.components.buttons
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.ac.tukorea.android.earalarm.ui.theme.EarAlarmTheme
+import kr.ac.tukorea.android.earalarm.ui.theme.Paddings
+
+@Composable
+fun PrimaryButton(
+    modifier: Modifier = Modifier,
+    @StringRes id: Int? = null,
+    text: String = "",
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.extraSmall,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.tertiary,
+            disabledContentColor = MaterialTheme.colorScheme.onTertiary
+        ),
+        enabled = enabled,
+        contentPadding = PaddingValues(horizontal = Paddings.large)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = id?.let { stringResource(id = id) } ?: text,
+                style = MaterialTheme.typography.labelSmall,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PrimaryButtonPreview() {
+    EarAlarmTheme {
+        PrimaryButton(text = "+1 hour") {}
+    }
+}
