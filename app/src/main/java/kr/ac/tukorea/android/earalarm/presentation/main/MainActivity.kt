6b2ab57e2112f885030120e95ee6a1a1159dcf2b
p-chanmin <pc_m111@naver.com>
2024-07-18 22:24:18 +0900

    feat: MainActivity compose 적용

--- app/src/main/java/kr/ac/tukorea/android/earalarm/presentation/main/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/android/earalarm/presentation/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package kr.ac.tukorea.android.earalarm.presentation.main
 
 import android.Manifest
-import android.app.AlertDialog
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -10,18 +8,13 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -34,18 +27,20 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.ac.tukorea.android.earalarm.R
 import kr.ac.tukorea.android.earalarm.databinding.ActivityMainBinding
-import kr.ac.tukorea.android.earalarm.databinding.DialogSettingBinding
 import kr.ac.tukorea.android.earalarm.presentation.alarm.AlarmHelper
-import kr.ac.tukorea.android.earalarm.presentation.main.model.MainUiEvent
+import kr.ac.tukorea.android.earalarm.presentation.main.output.MainUiEvent
+import kr.ac.tukorea.android.earalarm.presentation.main.output.ViewType
+import kr.ac.tukorea.android.earalarm.presentation.main.screen.AlarmScreen
+import kr.ac.tukorea.android.earalarm.presentation.main.screen.MeasuringScreen
+import kr.ac.tukorea.android.earalarm.presentation.main.viewmodel.MainViewModel
 import kr.ac.tukorea.android.earalarm.presentation.notification.NotificationHelper
 import kr.ac.tukorea.android.earalarm.presentation.service.AlarmPlayingService
-import kr.ac.tukorea.android.earalarm.ui.components.buttons.PrimaryButton
 import kr.ac.tukorea.android.earalarm.ui.theme.EarAlarmTheme
 import kr.ac.tukorea.android.earalarm.utils.toPath
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var alarmHelper: AlarmHelper
@@ -60,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     private val filePickerLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
             uri?.let {
-                it.toPath(this, mainViewModel.mainUiState.value.alarmMedia)
+                it.toPath(this, mainViewModel.alarmUiState.value.alarmMedia)
                     ?.let { path -> mainViewModel.setAlarmSound(path) }
             }
         }
@@ -76,61 +71,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        binding.viewModel = mainViewModel
-
-        binding.timePicker.setIs24HourView(true)
 
         notificationHelper.registerNotificationChannels()
-
         collectViewModelEvent()
-        loadInterstitialAd()
         askNotificationPermission()
+        loadInterstitialAd()
 
-        binding.composeViewTimeAdd.setContent {
+        setContent {
             EarAlarmTheme {
-                TimeAddButtons(viewModel = mainViewModel)
-            }
-        }
+                val viewType by mainViewModel.viewType.collectAsState()
 
-        binding.btnSetting.setOnClickListener {
-            val binding: DialogSettingBinding = DataBindingUtil.inflate<DialogSettingBinding?>(
-                layoutInflater, R.layout.dialog_setting, null, false
-            ).apply {
-                viewModel = mainViewModel
-                lifecycleOwner = this@MainActivity
-                setmedia.setOnClickListener { filePickerLauncher.launch("audio/*") }
-                volumeSeek.progress = mainViewModel.mainUiState.value.volume
-            }
+                when (viewType) {
+                    ViewType.ALARM -> {
+                        AlarmScreen(
+                            alarmUiState = mainViewModel.output.alarmUiState.collectAsState(),
+                            input = mainViewModel.input,
+                            confirmExactAlarmPermission = { confirmExactAlarmPermission() },
+                            confirmNotificationPermission = { confirmNotificationPermission() },
+                            audioFilePickerLauncher = { filePickerLauncher.launch("audio/*") }
+                        )
+                    }
 
-            AlertDialog.Builder(this).apply {
-                setTitle(getString(R.string.setting_alarm_text))
-                setIcon(R.mipmap.ic_earalarm_launcher)
-                setView(binding.root)
-                setPositiveButton(getString(R.string.setting_complete_text), null)
-            }.show()
-        }
-    }
-
-    @Composable
-    fun TimeAddButtons(viewModel: MainViewModel) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            PrimaryButton(id = R.string.timer_1_hour_plus) {
-                viewModel.updateTimePickerWithMinute(60)
-            }
-            PrimaryButton(id = R.string.timer_30_minute_plus) {
-                viewModel.updateTimePickerWithMinute(30)
-            }
-            PrimaryButton(id = R.string.timer_10_minute_plus) {
-                viewModel.updateTimePickerWithMinute(10)
-            }
-            PrimaryButton(id = R.string.timer_5_minute_plus) {
-                viewModel.updateTimePickerWithMinute(5)
+                    ViewType.MEASURING -> {
+                        MeasuringScreen(
+                            measuringUiState = mainViewModel.output.measuringUiState.collectAsState(),
+                            input = mainViewModel.input
+                        )
+                    }
+                }
             }
         }
     }
@@ -142,7 +110,6 @@ class MainActivity : AppCompatActivity() {
                     when (event) {
                         MainUiEvent.SetAlarm -> loadInterstitialAd()
                         MainUiEvent.DismissAlarm -> dismissAlarm()
-                        MainUiEvent.DeniedExactAlarm -> askExactAlarmPermission()
                     }
                 }
             }
@@ -179,64 +146,32 @@ class MainActivity : AppCompatActivity() {
             ) {
                 return
             } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                permissionDialog(context = this,
-                    title = R.string.permission_notification_title_request,
-                    message = R.string.permission_notification_message_request,
-                    positiveBtnText = R.string.permission_positive,
-                    negativeBtnText = R.string.permission_negative,
-                    positiveAction = {
-                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                    },
-                    negativeAction = {
-                        Toast.makeText(
-                            this@MainActivity,
-                            getString(R.string.permission_notification_denied),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }).show()
+                mainViewModel.showDeniedNotificationDialog()
             } else {
                 requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
 
-    private fun askExactAlarmPermission() {
+    private fun confirmExactAlarmPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permissionDialog(context = this,
-                title = R.string.permission_exact_alarm_title_request,
-                message = R.string.permission_exact_alarm_message_request,
-                positiveBtnText = R.string.permission_positive,
-                negativeBtnText = R.string.permission_negative,
-                positiveAction = {
-                    val intent = Intent(
-                        Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM,
-                        Uri.parse("package:${this.packageName}"),
-                    )
-                    intent.addCategory(Intent.CATEGORY_DEFAULT)
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    this.startActivity(intent)
-                },
-                negativeAction = {
-                    Toast.makeText(
-                        this, getString(R.string.permission_exact_alarm_denied), Toast.LENGTH_SHORT
-                    ).show()
-                }).show()
+            val intent = Intent(
+                Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM,
+                Uri.parse("package:${this.packageName}"),
+            ).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            this.startActivity(intent)
         }
     }
 
-    private fun permissionDialog(
-        context: Context,
-        @StringRes title: Int,
-        @StringRes message: Int,
-        @StringRes positiveBtnText: Int,
-        @StringRes negativeBtnText: Int,
-        positiveAction: () -> Unit,
-        negativeAction: () -> Unit
-    ): AlertDialog.Builder =
-        AlertDialog.Builder(context).setTitle(getString(title)).setMessage(getString(message))
-            .setPositiveButton(getString(positiveBtnText)) { _, _ ->
-                positiveAction()
-            }.setNegativeButton(getString(negativeBtnText)) { _, _ ->
-                negativeAction()
-            }
+    private fun confirmNotificationPermission() {
+        val intent = Intent(
+            Settings.ACTION_APP_NOTIFICATION_SETTINGS,
+        ).apply {
+            putExtra(Settings.EXTRA_APP_PACKAGE, this@MainActivity.packageName)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        this.startActivity(intent)
+    }
 }
