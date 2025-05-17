0b82a7d81f262d8364d36b965455edc96b7e7f1c
p-chanmin <pc_m111@naver.com>
2025-05-18 01:20:22 +0900

    v1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "kr.ac.tukorea.android.earalarm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.4.1"
+        versionCode = 15
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
