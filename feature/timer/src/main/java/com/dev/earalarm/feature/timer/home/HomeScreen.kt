fa37aaf5abbdef0c8e644a0c26add7bdf19879f3
p-chanmin <pc_m111@naver.com>
2025-05-17 23:45:25 +0900

    feat: InAppUpdate 기능 추가

--- feature/timer/src/main/java/com/dev/earalarm/feature/timer/home/HomeScreen.kt
+++ feature/timer/src/main/java/com/dev/earalarm/feature/timer/home/HomeScreen.kt
@@ -19,9 +19,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -56,6 +60,7 @@ import com.dev.earalarm.feature.timer.component.WheelPicker
 import com.dev.earalarm.feature.timer.model.HomeUiState
 import com.dev.earalarm.feature.timer.model.PermissionState
 import com.dev.earalarm.feature.timer.playservice.InAppReview
+import com.dev.earalarm.feature.timer.playservice.InAppUpdate
 import com.dev.firebase.LocalFirebaseManager
 import com.dev.firebase.model.FA
 import com.google.firebase.analytics.logEvent
@@ -74,6 +79,7 @@ internal fun HomeScreen(
     val homeUiState by homeViewModel.homeUiState.collectAsStateWithLifecycle()
     val firebaseManager = LocalFirebaseManager.current
     val configuration = LocalConfiguration.current
+    val flexibleSnackBarHostState = remember { SnackbarHostState() }
 
     LaunchedEffect(Unit) {
         firebaseManager.screenLogEvent("HomeScreen", configuration.orientation)
@@ -82,6 +88,11 @@ internal fun HomeScreen(
         }
     }
 
+    InAppUpdate(
+        snackBarHostState = flexibleSnackBarHostState,
+        isRejectFlexibleUpdate = homeUiState.isRejectFlexibleUpdate,
+        rejectFlexibleUpdate = homeViewModel::rejectFlexibleUpdate
+    )
 
     InAppReview(
         lastReviewDate = homeUiState.lastReviewDate,
@@ -98,6 +109,34 @@ internal fun HomeScreen(
         startTimerAlarm = homeViewModel::startTimerAlarm,
         updateTimerAlarm = homeViewModel::updateTimerAlarm,
     )
+
+    SnackbarHost(
+        hostState = flexibleSnackBarHostState,
+        modifier = Modifier.systemBarsPadding(),
+    ) { data ->
+        Snackbar(
+            modifier = Modifier
+                .padding(Paddings.small)
+                .padding(top = Paddings.medium),
+            action = {
+                data.visuals.actionLabel?.let {
+                    Text(
+                        modifier = Modifier
+                            .padding(end = Paddings.small)
+                            .clickable { data.performAction() },
+                        text = it,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = EarAlarmMaterialTheme.colorScheme.active
+                    )
+                }
+            }
+        ) {
+            Text(
+                text = data.visuals.message,
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+    }
 }
 
 @Composable
---
+++ feature/timer/src/main/java/com/dev/earalarm/feature/timer/playservice/InAppUpdate.kt
@@ -0,0 +1,186 @@
+package com.dev.earalarm.feature.timer.playservice
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.dev.earalarm.feature.timer.R
+import com.dev.firebase.LocalFirebaseManager
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
+import kotlinx.coroutines.launch
+
+private const val MIN_VERSION_DIFF_FOR_IMMEDIATE_UPDATE = 5L
+
+@Composable
+internal fun InAppUpdate(
+    snackBarHostState: SnackbarHostState,
+    isRejectFlexibleUpdate: Boolean,
+    rejectFlexibleUpdate: () -> Unit,
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val firebaseManager = LocalFirebaseManager.current
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val lifecycleState by lifecycle.currentStateFlow.collectAsStateWithLifecycle()
+
+    val appUpdateManager: AppUpdateManager = remember { AppUpdateManagerFactory.create(context) }
+
+    val appUpdateImmediateResultLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult(),
+    ) { result ->
+        val activity = context as? Activity
+        if (result.resultCode == Activity.RESULT_CANCELED) {
+            activity?.finish()
+        }
+    }
+
+    val appUpdateFlexibleResultLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult(),
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_CANCELED) {
+            rejectFlexibleUpdate()
+        }
+    }
+
+    val installStateUpdatedListener = remember {
+        InstallStateUpdatedListener { state ->
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                scope.launch {
+                    showFlexibleUpdateSnackBar(
+                        snackBarHostState,
+                        appUpdateManager,
+                        context.getString(R.string.feature_timer_update_complete),
+                        context.getString(R.string.feature_timer_update_install)
+                    )
+                }
+            }
+        }
+    }
+
+    DisposableEffect(Unit) {
+        appUpdateManager.registerListener(installStateUpdatedListener)
+        onDispose {
+            appUpdateManager.unregisterListener(installStateUpdatedListener)
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        try {
+            val versionCode = getAppVersionCode(context)
+            val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+            appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+                val availableImmediate =
+                    appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                            && appUpdateInfo.availableVersionCode()
+                        .toLong() - versionCode >= MIN_VERSION_DIFF_FOR_IMMEDIATE_UPDATE
+
+                val availableFlexible =
+                    appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+                            && !isRejectFlexibleUpdate
+
+                when {
+                    availableImmediate -> {
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            appUpdateImmediateResultLauncher,
+                            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+                        )
+                    }
+
+                    availableFlexible -> {
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            appUpdateFlexibleResultLauncher,
+                            AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
+                        )
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            firebaseManager.reportNonFatalError(e)
+        }
+    }
+
+    LaunchedEffect(lifecycleState) {
+        if (lifecycleState == Lifecycle.State.RESUMED) {
+            try {
+                val versionCode = getAppVersionCode(context)
+                val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+                appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+                    if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+                        && appUpdateInfo.availableVersionCode()
+                            .toLong() - versionCode >= MIN_VERSION_DIFF_FOR_IMMEDIATE_UPDATE
+                    ) {
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            appUpdateImmediateResultLauncher,
+                            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+                        )
+                    } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                        scope.launch {
+                            showFlexibleUpdateSnackBar(
+                                snackBarHostState,
+                                appUpdateManager,
+                                context.getString(R.string.feature_timer_update_complete),
+                                context.getString(R.string.feature_timer_update_install)
+                            )
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                firebaseManager.reportNonFatalError(e)
+            }
+        }
+    }
+}
+
+private suspend fun showFlexibleUpdateSnackBar(
+    snackBarHostState: SnackbarHostState,
+    appUpdateManager: AppUpdateManager,
+    message: String,
+    actionLabel: String,
+) {
+    val snackBarResult = snackBarHostState.showSnackbar(
+        message = message,
+        actionLabel = actionLabel,
+        duration = SnackbarDuration.Indefinite,
+    )
+
+    if (snackBarResult == SnackbarResult.ActionPerformed) {
+        appUpdateManager.completeUpdate()
+    }
+}
+
+private fun getAppVersionCode(context: Context): Long {
+    val packageInfo: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        packageInfo.longVersionCode
+    } else {
+        @Suppress("DEPRECATION")
+        packageInfo.versionCode.toLong()
+    }
+}
